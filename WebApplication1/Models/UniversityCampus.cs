5fdac7a9501fdff73df346baeec84e798eedeebb
Michael Zhao <Zhao@email.com>
2025-11-17 16:52:53 +0800

    web-t7

---
+++ WebApplication1/Models/UniversityCampus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class UniversityCampus
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Campus Name")]
+        public string Name { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+
+    }
+}
